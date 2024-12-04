ab66115fb2592ece3c2be0181341b93cc00ca6ce
Lars-Jørgen Kristiansen <1528255+LarsJK@users.noreply.github.com>
2024-12-04 06:40:33 +0100

    Test with request

--- formdata.test.ts
+++ formdata.test.ts
@@ -1,8 +1,20 @@
+// @vitest-environment jsdom
+
 import { test, expect } from "vitest";
 
-test("form data file", () => {
-  const file = new File(["hello"], "hello.png", { type: "image/png" });
+test("form data file", async () => {
+  const file = new File(["{}"], "hello.json", { type: "application/json" });
   const formData = new FormData();
   formData.append("file", file);
+
+  const request = new Request("http://localhost:3000", {
+    method: "POST",
+    body: formData,
+  });
+
+  const text = await request.text();
+
+  console.log(text);
+
   expect(formData.get("file")).toBeDefined();
 });
